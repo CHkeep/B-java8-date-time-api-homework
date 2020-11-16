bd46d2899bd07bc167c48d9cfa5925fde0d7fe59
Hui <hui.chen@thoughtworks.com>
2020-11-16 21:59:18 +0800

    add pricetise1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +14,12 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int currentYear = date.getYear();
+
+    LocalDate laborDayTime = date.getMonthValue() < 5 ? LocalDate.of(currentYear + 0,5,1): LocalDate.of(currentYear+1,5,1);
+    Period next = Period.between(date, laborDayTime);
+//    int day = next.getDays();
+    int day = Math.abs(Math.toIntExact(ChronoUnit.DAYS.between(laborDayTime, date)));
+    return day;
   }
 }
